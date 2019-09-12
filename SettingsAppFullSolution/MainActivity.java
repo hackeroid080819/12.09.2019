69f2b92af15b80fe3a987336ea24778c5113ea35
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-09-12 14:33:33 +0300

    Update MainActivity.java

--- SettingsAppFullSolution/MainActivity.java
+++ SettingsAppFullSolution/MainActivity.java
@@ -20,6 +20,12 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        displayShared();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -53,6 +59,31 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private void displayShared()
+    {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String username = prefs.getString("username", "Default NickName");
+        boolean updates = prefs.getBoolean("applicationUpdates", false);
+        String st = prefs.getString("downloadDetials", "1");
+        int downloadDetialsNumber = 1;
+        try {
+            //int downloadDetialsNumber = prefs.getInt("downloadDetials", 1);
+            downloadDetialsNumber = Integer.parseInt(st);
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(getBaseContext(), "Cannot convert " + st + " to int", Toast.LENGTH_SHORT).show();
+        }
+
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Username: " + username + "\n");
+        builder.append("Updates: " + updates+ "\n");
+        builder.append("Download details: " + getResources().
+                getStringArray(R.array.listArray)[downloadDetialsNumber - 1]);
+        TextView sttv = findViewById(R.id.settingsTV);
+        sttv.setText(builder.toString());
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -72,27 +103,7 @@ public class MainActivity extends AppCompatActivity {
         }
         if (id == R.id.action_settings_display) {
 
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-            String username = prefs.getString("username", "Default NickName");
-            boolean updates = prefs.getBoolean("applicationUpdates", false);
-            String st = prefs.getString("downloadDetials", "1");
-            int downloadDetialsNumber = 1;
-            try {
-                //int downloadDetialsNumber = prefs.getInt("downloadDetials", 1);
-                downloadDetialsNumber = Integer.parseInt(st);
-            }
-            catch (Exception ex)
-            {
-                Toast.makeText(getBaseContext(), "Cannot convert " + st + " to int", Toast.LENGTH_SHORT).show();
-            }
-
-
-            StringBuilder builder = new StringBuilder();
-            builder.append("Username: " + username + "\n");
-            builder.append("Updates: " + updates+ "\n");
-            builder.append("Download details: " + getResources().getStringArray(R.array.listArray)[downloadDetialsNumber - 1]);
-            TextView sttv = findViewById(R.id.settingsTV);
-            sttv.setText(builder.toString());
+          displayShared();
             return true;
         }
 
